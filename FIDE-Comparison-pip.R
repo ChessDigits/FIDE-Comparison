cbedcf50aa23e1b8341f023f0dc6dbdacd2aa294
Patrick Coulombe <patrick.coulombe@gmail.com>
2021-03-17 15:07:42 -0400

    Add nthreads argument to call to init_h2o() using config

--- FIDE-Comparison-pip.R
+++ FIDE-Comparison-pip.R
@@ -11,7 +11,7 @@ pip
 df <- load_data()
 df <- remove_username(df)
 tt <- ttsplit(df)
-init_h2o()
+init_h2o(nthreads = config$h2o$nthreads)
 tt_h2o <- ship_train_and_test_to_h2o(tt)
 automl <- get_automl_model(config, tt_h2o)
 tt <- add_predictions_from_automl(automl, tt, tt_h2o, add_confidence_level = F)
