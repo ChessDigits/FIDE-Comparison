47785c049b9b2d8fa782ccb45c1c6d01904b21bd
Patrick Coulombe <patrick.coulombe@gmail.com>
2021-03-17 14:49:01 -0400

    Add config, ship to h2o, get automl, and add predictions to test

--- FIDE-Comparison-fn.R
+++ FIDE-Comparison-fn.R
@@ -8,6 +8,20 @@ https://github.com/EthanLebowitz/ChessData/blob/master/FIDEreportedLichess.csv
 fn
 "
 
+#### config ####
+config <- list(
+  target="FIDE_rating",
+  h2o=list(
+    nthreads=c("all", "minus1", "half")[2]
+    ,algos="DRF"
+    ,max_runtime_secs = NULL
+    ,max_models=1
+    ,max_runtime_secs_per_model=60*5
+    ,balance_classes=FALSE
+  )
+)
+
+
 #### imports ####
 library(h2o)
 
@@ -70,5 +84,80 @@ init_h2o <- function(nthreads=c("minus1", "half", "all"))
   # init
   nthreads <- nthreads_values[[nthreads]]
   h2o.init(nthreads=nthreads, max_mem_size = paste0(floor(.7*16), "g"))
+  
+}
+
+ship_train_and_test_to_h2o <- function(tt)
+{
+  "
+  input: tt list (train and  test)
+  output: h2o pointers
+  "
+  
+  # train-test
+  train <- as.h2o(tt$train, "train")
+  test <- as.h2o(tt$test, "test")
+  print("Destination frames added: 'train' and 'test'")
+  
+  # check
+  print("Row check:")
+  print(paste0("H2O dfs: train = ", nrow(as.data.frame(train)), ", test = ", nrow(as.data.frame(test))))
+  print(paste0("R dfs: "))
+  print(lapply(tt, nrow))
+  
+  # out
+  return(list(train=train, test=test))
+  
+}
+
+# automl
+get_automl_model <- function(config, tt_h2o)
+{
+  "
+  input: tt_h2o is list of h2o pointers, but only $train is used
+  return automl object
+  "
+  
+  # model
+  automl <- h2o.automl(
+    y=config$target,
+    training_frame = tt_h2o$train,
+    stopping_tolerance = .01,
+    include_algos = config$h2o$algos,
+    keep_cross_validation_predictions = F,
+    keep_cross_validation_models = F,
+    max_runtime_secs = config$h2o$max_runtime_secs
+    ,max_models = config$h2o$max_models, max_runtime_secs_per_model = config$h2o$max_runtime_secs_per_model
+    ,balance_classes=config$h2o$balance_classes
+  )
+  
+  # out
+  return(automl)
+}
+
+# add predictions
+add_predictions_from_automl <- function(automl, tt, tt_h2o, add_confidence_level=TRUE)
+{
+  "
+  return tt with added column to test and confidence level (if requested)
+  "
+  
+  leader_pred <- h2o.predict(automl, newdata = tt_h2o$test)
+  leader_pred_df <- as.data.frame(leader_pred)
+  colnames(leader_pred_df) <- colnames(leader_pred)
+  df_confidences <- leader_pred_df[,-1]
+  
+  tt$test$predict <- leader_pred_df$predict
+  
+  # out
+  print("Added predict column to test")
+  
+  # confidence levels if requested
+  if (add_confidence_level) tt$test$confidence <- apply(df_confidences, 1, max)
+  print("Added confidence column to test")
+  
+  
+  # return
+  return(tt)
   
 }
